7018405ac662b315e5a7dbded88896e76a981470
Kazuyuki Honda <hakobera@gmail.com>
2014-01-20 23:11:29 +0900

    Save image to Amazon S3

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 var koa = require('koa');
+var route = require('koa-route');
+var logger = require('koa-logger');
+var parse = require('co-busboy');
+var uuid = require('node-uuid');
+
 var render = require('./lib/render');
+var save = require('./lib/save');
 
 var app = module.exports = koa();
+var host = process.env.HOST || 'http://localhost:3000';
 var port = process.env.PORT || 3000;
 
 // Middleware
 
+app.use(logger());
+
 // Routes
 
-app.use(function *() {
+app.use(route.get('/', function *() {
   this.body = yield render('index');
-});
+}));
+
+app.use(route.post('/upload', function *() {
+  var parts = parse(this, {
+    autoFields: true
+  });
+  var id = uuid.v1();
+  var part, image;
+
+  while (part = yield parts) {
+    image = yield save(id, part);
+    break; // only process one image file
+  }
+
+  this.body = host + '/' + image.key;
+}));
 
 // Error handler
 
 app.use(function *(next) {
   try {
-    yield next();
+    yield next;
   } catch (err) {
     this.throw(500, err.message);
   }
@@ -30,6 +54,6 @@ app.on('error', function (err) {
 
 if (!module.parent) {
   app.listen(port, function () {
-    console.log('app running on %d', port);
+    console.log('app listening on port %d', port);
   });
 }
