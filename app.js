d73ebd59966d2afae4f0733ce7f31da079fb3a13
Kazuyuki Honda <hakobera@gmail.com>
2014-01-20 21:46:30 +0900

    Add index page

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 var koa = require('koa');
+var render = require('./lib/render');
 
 var app = module.exports = koa();
 var port = process.env.PORT || 3000;
 
-app.listen(port, function () {
-  console.log('app running on %d', port);
+// Middleware
+
+// Routes
+
+app.use(function *() {
+  this.body = yield render('index');
 });
+
+// Error handler
+
+app.use(function *(next) {
+  try {
+    yield next();
+  } catch (err) {
+    this.throw(500, err.message);
+  }
+});
+
+app.on('error', function (err) {
+  console.error(err.stack);
+});
+
+// Run server
+
+if (!module.parent) {
+  app.listen(port, function () {
+    console.log('app running on %d', port);
+  });
+}
